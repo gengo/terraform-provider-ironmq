ef188565a317940eda8144c10ecff7ce88fe67e3
Yuki Yugui Sonoda <yugui@gengo.com>
2015-12-22 13:02:38 +0900

    Keep states
    It was necessary to call SetId to let Terraform save states of resources

--- resource_queue.go
+++ resource_queue.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/iron-io/iron_go3/config"
 	"github.com/iron-io/iron_go3/mq"
@@ -29,14 +31,23 @@ func createQueue(data *schema.ResourceData, meta interface{}) error {
 	_, err := mq.ConfigCreateQueue(mq.QueueInfo{
 		Name: name,
 	}, &cfg)
-	return err
+	if err != nil {
+		return err
+	}
+	if err := readQueue(data, meta); err != nil {
+		return err
+	}
+	return nil
 }
 
 func updateQueue(data *schema.ResourceData, meta interface{}) error {
 	return nil
 }
 
-func readQueue(data *schema.ResourceData, ironcfg interface{}) error {
+func readQueue(data *schema.ResourceData, meta interface{}) error {
+	cfg := meta.(config.Settings)
+	name := data.Get("name").(string)
+	data.SetId(fmt.Sprintf("%s/%s", cfg.ProjectId, name))
 	return nil
 }
 
